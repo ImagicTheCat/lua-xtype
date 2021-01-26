cfaf1f673ea18370d4fd792c4260ca3fab5569ba
ImagicTheCat <imagicthecat@gmail.com>
2021-01-26 21:30:37 +0100

    Minor example fix.

--- examples/bench_check.lua
+++ examples/bench_check.lua
@@ -8,6 +8,7 @@ n = tonumber(n) or 1e7
 local T = xtype.create("T")
 local t = setmetatable({}, {xtype = T})
 
+local a
 for i=1,n do
   a = xtype.is(T, "xtype") and xtype.is(t, T) and xtype.is(i, "number")
 end
